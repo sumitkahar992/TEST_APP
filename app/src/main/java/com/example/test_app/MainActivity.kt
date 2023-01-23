ea7af53fb95125db60f04301506cf9d58a56ec79
sumitkahar992 <sumitkahar073@gmail.com>
2023-01-23 23:15:50 +0545

    lot of functions added in kotlin module..

--- app/src/main/java/com/example/test_app/MainActivity.kt
+++ app/src/main/java/com/example/test_app/MainActivity.kt
@@ -1,40 +1,55 @@
 package com.example.test_app
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.test_app.ui.theme.TEST_APPTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TEST_APPTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+//            ExampleScreen()
+            SearchLists()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    TEST_APPTheme {
-        Greeting("Android")
+fun ExampleScreen() {
+
+    var count by remember { mutableStateOf(0) }
+
+    val calculation by remember {
+        derivedStateOf {
+            Log.d("LOG", "Calculated")
+            count > 3
+        }
+    }
+
+    Log.d("LOG", "READ: $calculation")
+//    Log.d("LOG", "READ: ${count > 3} ")
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {
+            Log.d("LOG", "Clicked")
+            count += 1
+        }) {
+            Text(text = "Increment")
+        }
+
     }
 }
